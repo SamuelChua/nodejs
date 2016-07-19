7b05bf7cb3b1e61a23a6ac8e87af6ceca22e8842
justin-tt <justin.ch.goh+tt@gmail.com>
2016-07-20 00:51:43 +0800

    testing port 80

--- indexCounter.js
+++ indexCounter.js
@@ -16,4 +16,5 @@ var server = http.createServer(function (request, response) {
 });
 
 console.log("Server running at http://127.0.0.1:8080");
-server.listen(process.env.PORT || 8080);
+// server.listen(process.env.PORT || 8080);
+server.listen(80);
