a099c898037dbafb561c63c590cb4709e148aede
justin-tt <justin.ch.goh+tt@gmail.com>
2016-07-19 23:45:41 +0800

    added index.js
    node.js hello world server

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    // use an if statement to check request.url
+    // if url is "/", respond with hello world
+    // else respond with 404 not found
+    if (request.url === '/') {
+        response.end("Hello World!");
+    } else {
+        response.end("404 NOT FOUND");
+    }
+
+});
+
+console.log("Server running at http://127.0.0.1:8000");
+server.listen(8000);
