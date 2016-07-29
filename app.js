ba0037493797d423f98289e155a4e38f4c2c3e3f
SamuelChua <chua.jiacong.samuel@dhs.sg>
2016-07-29 16:40:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ $(function() {
     $("#getJoke").click(
         function() {
             $.get("/jokes",function(data){
-                $("#setup").html(data.setup);
-                $("#punchline").html(data.punchline);
+                alert(data.setup);
+                alert(data.punchline);
             },"json")
         }
         $('#upvote').on("click", function(){
